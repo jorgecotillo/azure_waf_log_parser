6911cba6184928283937e014f6719a6cc32b44ff
Jorge Cotillo <jorgecotillo@Jorges-MacBook-Pro-3.local>
2017-12-25 23:09:22 -0500

    Updated code, made it more readable

--- ClientApp/app/components/parser/parser.component.ts
+++ ClientApp/app/components/parser/parser.component.ts
@@ -11,43 +11,36 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 export class ParserComponent implements OnInit{
 
     public _blobs: Array<string> = new Array<string>(); 
+
     ngOnInit() {
-        console.log("On Init");
-        var x: Array<string> = new Array<string>();
         this.getLogData().then(value => {
-            console.log("after promise", value);
+            //console.log("after promise", value); 
+            // Getting evaluated array because it came from resolve()
             this._blobs = value;
         });
     }
 
-    getLogData(/*callback: (resultData: Array<string>) => void*/) {
+    getLogData() {
         var blobService = createBlobService(environment.AZURE_STORAGE_CONNECTION_STRING);
         var blobOptions: BlobService.ListBlobsSegmentedRequestOptions = { delimiter : 'y=2017/m=12/d=04' };
         return new Promise<Array<string>>( (resolve, reject) => {
+            var blobs: Array<string> = new Array<string>();
             blobService
             .listBlobsSegmentedWithPrefix(
                 'insights-logs-applicationgatewayfirewalllog', 
-                'resourceId=/SUBSCRIPTIONS/XXXXXXX/RESOURCEGROUPS/DTPORTALSPROD/PROVIDERS/MICROSOFT.NETWORK/APPLICATIONGATEWAYS/XXXXXX/y=2017/m=12/d=04',
+                'resourceId=/SUBSCRIPTIONS/XXXXX/RESOURCEGROUPS/DTPORTALSPROD/PROVIDERS/MICROSOFT.NETWORK/APPLICATIONGATEWAYS/XXXX/y=2017/m=12/d=04',
                 null,
-                (error: Error, result: BlobService.ListBlobsResult, response: ServiceResponse) => { 
-                    var blobs: Array<string> = new Array<string>();
-                    
-                    var loopPromise = new Promise<Array<string>>((resolve, reject) =>{
-                        result.entries.forEach((entry: BlobService.BlobResult) => {
-                            blobs.push(entry.name);
-                        });
-                        resolve(blobs);
+                (error: Error, result: BlobService.ListBlobsResult, response: ServiceResponse) => {
+                    result.entries.forEach((entry: BlobService.BlobResult) => {
+                        blobs.push(entry.name);
                     });
-
-                    loopPromise.then(value => {
-                        console.log("finished loop", value);
-                        resolve(blobs); //NOTE: have to return this value again so it can be picked up
-                                        //      by caller
-                    }, reason => {
-                        console.log(reason);
-                    } );
+                    resolve(blobs); 
+                    // By executing resolve(), the caller will get the evaluated array
                  },
             );
+            //console.log("Unevaluated array", blobs); 
+            //Chrome shows zero results until the array is opened 
+            //(Chorme message: Below value was evaluated just now)
         });
     }
 }
